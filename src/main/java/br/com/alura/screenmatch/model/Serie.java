634746ae8735fe324a5f87c89779f96c2a7e660f
gustavorch <grself@tutanota.com>
2024-04-04 14:44:46 -0300

    Criando o enum Categoria e adquirindo novos elementos em dadosSerie

---
+++ src/main/java/br/com/alura/screenmatch/model/Serie.java
@@ -0,0 +1,26 @@
+package br.com.alura.screenmatch.model;
+
+import java.util.OptionalDouble;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+
+public class Serie {
+    private String titulo;
+    private Integer totalTemporadas;
+    private Double avaliacao;
+    private Categoria genero;
+    private String atores;
+    private String sinopse;
+    private String poster;
+
+    public Serie(DadosSerie dadosSerie) {
+        this.titulo = dadosSerie.titulo();
+        this.totalTemporadas = dadosSerie.totalTemporadas();
+        this.avaliacao = OptionalDouble.of(Double.valueOf(dadosSerie.avaliacao())).orElse(0);
+        this.genero = Categoria.fromString(dadosSerie.genero().split(",")[0].trim());
+        this.atores = dadosSerie.atores();
+        this.sinopse = dadosSerie.sinopse();
+        this.poster = dadosSerie.poster();
+
+    }
+}
